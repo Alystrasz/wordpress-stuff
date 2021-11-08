95974c4cb0684590b728bd6977b7f442ec4ff28b
Remy Raes <contact@remyraes.com>
2021-11-08 21:09:03 +0100

    [docs] updating plugin description

--- plugins/papi-carousel/papi-carousel.php
+++ plugins/papi-carousel/papi-carousel.php
@@ -1,11 +1,12 @@
 <?php
 /**
  * Plugin Name:       Papi Carousel
- * Description:       Example block written with ESNext standard and JSX support – build step required.
+ * Description:       Affiche les derniers articles publiés sous la forme d'un carousel beaucoup trop stylé.
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Rémy Raes
+ * Author URI:        https://remyraes.com/
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       papi-carousel
